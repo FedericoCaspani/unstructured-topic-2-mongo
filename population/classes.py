47d6a4cfa8fd25ef53cffb4910decb2cdcbda5f7
matteocitterio <10620055@polimi.it>
2021-12-03 20:53:21 +0100

    documents generator

---
+++ population/classes.py
@@ -0,0 +1,193 @@
+import json
+from random import randint
+from typing import List
+
+
+# &&&&&&&& CLASSES &&&&&&&&
+
+
+# class for the emergency contact
+class EmergencyContact:
+    name: str
+    email: str
+    relationship: str
+
+    def __init__(self, name, email, relationship):
+        self.name = name
+        self.email = email
+        self.relationship = relationship
+
+    def print(self):
+        print("Emergency Contact: " + self.name + " - " + self.email + " (" + self.relationship + ")")
+
+
+# class for containing both doctors and nurses
+class Worker:
+    name: str
+    title: str
+
+    def __init__(self, name, title):
+        self.name = name
+        self.title = title
+
+    @staticmethod
+    def print(self):
+        print(self.name + " - " + self.title)
+
+
+# class for containing all the needed information about authorized body
+class Place:
+    name: str
+    entity: str
+    address: str
+    gps: str
+    departments: []
+
+    def __init__(self, name, gps, address, entity):
+        self.name = name
+        self.entity = entity
+        self.address = address
+        self.gps = gps
+        self.departments = self.gen_departments()
+
+    @staticmethod
+    def gen_departments():
+        # all possible departments
+        departments = ["DT.1", "DT.2", "DT.3", "D1.1", "D1.2", "D2.1", "D2.2", "D2.3", "D2.4", "D3.1"]
+        # generate only some floors
+        possible_combs = [3, 5, 8, 10]
+        combination = randint(0, len(possible_combs) - 1)
+        return departments[:possible_combs[combination]]
+
+    def print(self, adder):
+        print(adder + "Authorized body:")
+        print(adder + "Name: " + str(self.name))
+        print(adder + "Type of entity: " + str(self.entity))
+        print(adder + "Address | Coordinates: " + str(self.address) + " | " + str(self.gps))
+        print(adder + "Departments: " + str(self.departments))
+        print()
+
+    def clone(self):
+        clone = Place(self.name, self.gps, self.address, self.entity)
+        return clone
+
+    def set_as_single_department(self):
+        chosen = randint(0, len(self.departments)-1)
+        self.departments = self.departments[chosen]
+
+
+# class for modeling Vaccines
+class Vaccine:
+    date: str
+    brand: str
+    type: str
+    lot: str
+    production_date: str
+    place: Place
+    doctors: List[Worker] = []
+    nurses: List[Worker] = []
+
+    def __init__(self, date, info, place, doctors, nurses):
+        self.date = date
+        self.brand = info[0]
+        self.type = info[1]
+        self.lot = info[2]
+        self.production_date = info[3]
+        self.place = place
+        self.doctors = []
+        self.doctors.append(doctors)
+        self.nurses = []
+        self.nurses.append(nurses)
+
+    def print(self, adder):
+        print(adder + "Vaccine done in date: " + self.date)
+        print(
+            adder + "Brand: " + self.brand + ", Type: " + self.type + ", Lot: " + self.lot + ", Production date: " + self.production_date)
+        self.place.print(adder="        ")
+        print(adder + "Doctors: [")
+        for d in self.doctors:
+            print(adder + "" + adder + "" + d.name + " - " + d.title)
+        print(adder + "]")
+        print(adder + "Nurses: [")
+        for n in self.nurses:
+            print(adder + "" + adder + "" + n.name + " - " + n.title)
+        print(adder + "]")
+
+
+# class for modeling the Tests
+class Test:
+    date: str
+    type: str
+    result: str
+    place: Place
+    doctors: List[Worker] = []
+    nurses: List[Worker] = []
+
+    def __init__(self, date, type, result, place, doctors, nurses):
+        self.date = date
+        self.type = type
+        self.result = result
+        self.place = place
+        self.doctors = []
+        self.doctors.append(doctors)
+        self.nurses = []
+        self.nurses.append(nurses)
+
+    def print(self, adder):
+        print(adder + "Test done in date: " + self.date)
+        print(adder + "Type: " + self.type)
+        self.place.print(adder="        ")
+        print(adder + "Doctors: [")
+        for d in self.doctors:
+            print(adder + "" + adder + "" + d.name + " - " + d.title)
+        print(adder + "]")
+        print(adder + "Nurses: [")
+        for n in self.nurses:
+            print(adder + "" + adder + "" + n.name + " - " + n.title)
+        print(adder + "]\n")
+
+
+# Main class
+class Person:
+    name: str
+    taxCode: str
+    age: int
+    emergency_contact: EmergencyContact
+    vaccines: List[Vaccine] = []
+    tests: List[Test] = []
+
+    def __init__(self, name, taxCode, age, emergency_contact):
+        self.name = name
+        self.taxCode = taxCode
+        self.age = age
+        self.emergency_contact = emergency_contact
+        self.vaccines = []
+        self.tests = []
+
+    def set_vaccines(self, vaccines):
+        for v in vaccines:
+            self.vaccines.append(v)
+
+    def set_tests(self, tests):
+        for t in tests:
+            self.tests.append(t)
+
+    def print(self):
+        print("PERSON")
+        print("Name: " + self.name)
+        print("Tax Code: " + self.taxCode)
+        print("Age: " + str(self.age))
+        self.emergency_contact.print()
+        print("Vaccines: [")
+        for v in self.vaccines:
+            v.print(adder="    ")
+        print("]")
+        print("Tests: [")
+        for t in self.tests:
+            t.print(adder="    ")
+        print("]")
+        print("\n\n\n\n")
+
+    def toJSON(self):
+        return json.dumps(self, default=lambda o: o.__dict__,
+                          sort_keys=True, indent=4)
---
+++ population/dbPopulator.py
@@ -0,0 +1,139 @@
+from classes import *
+from params import *
+
+from pymongo import MongoClient
+import pandas as pd
+from random import randint
+
+global db
+
+
+# local uri: "mongodb+srv://root:password*qwerty@cluster0.63i2t.mongodb.net/test"
+
+def get_db():
+    client = MongoClient(CONNECTION_STRING)
+    return client["swagbro"]
+
+
+def get_doctors():
+    names = pd.read_csv("dataset/names.csv", nrows=NUM_DOCTORS)
+    names = names.values[:, 0]
+    titles = ["Doctor", "Specialist", "Anesthetist"]
+    docs = []
+    for i in range(len(names)):
+        docs.append(Worker(names[i], titles[i % 3]))
+
+    return docs
+
+
+def get_nurses():
+    names = pd.read_csv("dataset/names.csv", skiprows=NUM_DOCTORS, nrows=NUM_NURSES)
+    names = names.values[:, 0]
+    titles = ["Nurse", "Assistant"]
+    nurses = []
+    for i in range(len(names)):
+        nurses.append(Worker(names[i], titles[i % 2]))
+
+    return nurses
+
+
+def get_auth_bodies():
+    places = pd.read_csv("dataset/places.csv", nrows=NUM_PLACES)
+
+    # extracting columns as parameters from csv file
+    names = places["Name"].values
+    coordinates = places["Coordinates"].values
+    addresses = places["Address"].values
+    types = places["Entity"].values
+
+    # creating Place objects and inserting them in a list structure to return
+    bodies = []
+    for i in range(NUM_PLACES):
+        bodies.append(Place(names[i], coordinates[i], addresses[i], types[i]))
+
+    return bodies
+
+
+def generate_emergency_contacts():
+    names = pd.read_csv("dataset/names.csv", skiprows=NUM_DOCTORS + NUM_NURSES + NUM_PEOPLE, nrows=NUM_PEOPLE)
+    names = names.values[:, 0]
+    relationships = ["Family contact", "Friend", "Partner"]
+
+    contacts = []
+    for i in range(NUM_PEOPLE):
+        contacts.append(EmergencyContact(names[i], "email" + str(i) + "@domain.com", relationships[randint(0, 2)]))
+
+    return contacts
+
+
+def generate_vaccines(doctors, nurses, auth_bodies):
+    brands = ["Pfizer", "Astrazeneca", "Sputnik V", "Johnson&Johnson", "Moderna"]
+    types = ["Inactivated", "mRNA", "Viral Vector", "Toxoid"]
+    lots = [["1", 2021, 4, 5], ["2", 2021, 11, 1], ["3", 2021, 9, 21], ["4", 2021, 1, 25], ["5", 2021, 6, 19]]
+
+    vaccines = []
+    for i in range(1, randint(MIN_V, MAX_V)):
+        sel_lot = lots[randint(0, len(lots) - 1)]
+        date = str(sel_lot[1]) + "-" + str(sel_lot[2] + 1) + "-" + str(sel_lot[3])
+        production_date = str(sel_lot[1]) + "-" + str(sel_lot[2]) + "-" + str(sel_lot[3])
+        info = [brands[randint(0, len(brands) - 1)], types[randint(0, len(types) - 1)], str(sel_lot[0]),
+                production_date]
+
+        auth_body = auth_bodies[randint(0, len(auth_bodies) - 1)].clone()
+        auth_body.set_as_single_department()
+
+        vaccines.append(Vaccine(date=date, info=info, place=auth_body,
+                                doctors=doctors[randint(0, len(doctors) - 1)],
+                                nurses=nurses[randint(0, len(nurses) - 1)]))
+
+    return vaccines
+
+
+def generate_tests(doctors, nurses, auth_bodies):
+    types = ["NAAT", "Antigen"]
+    lots = [["1", "2021-04-05"], ["2", "2021-11-01"], ["3", "2021-09-21"], ["4", "2021-01-25"], ["5", "2021-06-19"]]
+
+    tests = []
+    for i in range(MIN_T, MAX_T):
+        date = "2021-" + str(randint(1, 12)) + "-" + str(randint(1, 28))
+        result = "Negative" if (randint(0, 100) / 100) <= (1 - POSITIVE_PROBABILITY) else "Positive"
+
+        tests.append(Test(date, types[randint(0, len(types) - 1)], result,
+                          place=auth_bodies[randint(0, len(auth_bodies) - 1)],
+                          doctors=doctors[randint(0, len(doctors) - 1)], nurses=nurses[randint(0, len(nurses) - 1)]))
+
+    return tests
+
+
+def generate_people(doctors, nurses, auth_bodies, emergency_contacts):
+    names = pd.read_csv("dataset/names.csv", skiprows=NUM_DOCTORS + NUM_NURSES, nrows=NUM_PEOPLE)
+    names = names.values[:, 0]
+
+    people = []
+    for i in range(NUM_PEOPLE):
+        # Creating the person still without tests/vaccines
+        person = Person(name=names[i], taxCode=str(i + 1), age=randint(14, 80), emergency_contact=emergency_contacts[i])
+        # Separately setting vaccines and tests
+        person.set_vaccines(generate_vaccines(doctors, nurses, auth_bodies))
+        person.set_tests(generate_tests(doctors, nurses, auth_bodies))
+        people.append(person)
+
+    return people
+
+
+if __name__ == "__main__":
+
+    # GENERATING AUTOMATIC DATA
+    doctors = get_doctors()
+    nurses = get_nurses()
+    auth_bodies = get_auth_bodies()
+    emergency_contacts = generate_emergency_contacts()
+
+    people = generate_people(doctors, nurses, auth_bodies, emergency_contacts)
+
+    # CONNECTING & INSERTING ON MONGO DATABASE ALL THE GENERATED PEOPLE IN JSON FORMAT
+    db = get_db()
+    people_collection = db.get_collection("people_registration")
+    for p in people:
+        data_to_write = json.loads(p.toJSON())
+        people_collection.insert_one(data_to_write)
